ec0fcdf6b7cdd4e162832fed3c9d4bed95ad7b93
yinzf2 <yinzf2@liandisys.com.cn>
2017-09-19 14:40:13 +0800

    to Github LearningPrj

---
+++ src/test/java/com/yinzifan/liandisys/SpringJDBCTemplateQueryTest.java
@@ -0,0 +1,21 @@
+package com.yinzifan.liandisys;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.yinzifan.liandisys._0918_spring03_JDBCDemo.dao.CustomerDAO;
+
+/**
+ * @author yinzf2
+ * 2017/09/19	10:19:58
+ */
+public class SpringJDBCTemplateQueryTest {
+	@Test
+	public void testSpringJDBCTemplateQuery() throws Exception {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("_0919_spring05_ApplicationContext.xml");
+		CustomerDAO dao = (CustomerDAO) ac.getBean("customerDAO");
+		dao.findByCustomerId(1);
+	}
+
+}
