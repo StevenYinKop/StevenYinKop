42070ab4189cb0f81aa5550f3f16a2dc782caefb
yinzf2 <yinzf2@liandisys.com.cn>
2017-09-20 15:05:16 +0800

    SpringJDBCFinal Commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # CinCommon
 THIS IS MY FIRST REPOSITORY
 ラーニングのレコードにある
+Spring フレームワーク　を　記録しています。
---
+++ src/test/java/com/yinzifan/liandisys/SpringFinal_JdbcDemoTest.java
@@ -0,0 +1,28 @@
+package com.yinzifan.liandisys;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.yinzifan.liandisys._0920_SpringFinal_JdbcDemo.bean.Department;
+import com.yinzifan.liandisys._0920_SpringFinal_JdbcDemo.dao.DepartmentDAO;
+
+/**
+ * @author yinzf2
+ * 2017/09/20	14:57:59
+ */
+public class SpringFinal_JdbcDemoTest {
+	@Test
+	public void testJdbcDemo() throws Exception {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("_0920_springfinal_ApplicationContext.xml");
+		DepartmentDAO dao = (DepartmentDAO) ac.getBean("DepartmentDAO");
+		List<Department> departments = dao.queryDepartment();
+		for (Department department : departments) {
+			System.out.println(department.toString());
+		}
+	}
+}
